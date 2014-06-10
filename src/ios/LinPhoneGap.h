0f0e3050d8411119e7f3628e89dfa645757526a0
johnroy <john@babelroom.com>
2014-06-10 09:48:37 -0700

    Update LinPhoneGap.h

--- src/ios/LinPhoneGap.h
+++ src/ios/LinPhoneGap.h
@@ -2,8 +2,8 @@
 //  LinPhoneGap.h
 //
 //  Created by John Roy on 04/01/2014
+//  Some code copied from linphone under GPL. See LICENSE.gpl. linphone.org
 //  Copyright (c) 2014 BabelRoom. All rights reserved.
-
 
 #import <Cordova/CDV.h>
 
